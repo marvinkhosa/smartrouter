7b39ddcc6bace144aca0569560a019cd74afb6d6
unknown <marvinkhosaambition@gmail.com>
2021-11-10 12:25:48 +0200

    deploy into heroku

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+const exphbs  = require('express-handlebars');
 const express = require("express")
 const app = express()
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(express.static('public'));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.get('/', function(request, response) {
-	response.send("fleet")
+	response.render("index")
 });
 
 const port = process.env.PORT||3000 
 app.listen(port,function(){
     console.log('App running')
 })
+
