619f91c5646d2be28658e5fe7012c5e16427bcf0
unknown <marvinkhosaambition@gmail.com>
2021-11-10 12:38:21 +0200

    smartrouter

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static('public'));
+app.use(express.static('public'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
